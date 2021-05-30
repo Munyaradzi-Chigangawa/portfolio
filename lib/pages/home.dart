f1284020179838ec965db4e2e230e4afe86545a7
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-05-30 02:32:55 +0200

    added home

---
+++ lib/pages/home.dart
@@ -0,0 +1,417 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:portfolio/utils/linkicon.dart';
+import 'about.dart';
+import 'contact_us.dart';
+import 'footer.dart';
+import 'header.dart';
+import 'my_projects.dart';
+import 'responsive_widget.dart';
+import 'statistics.dart';
+import 'work_plan.dart';
+
+
+class Home extends StatefulWidget {
+  const Home({ Key key }) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _headerGlobalKey = GlobalKey();
+  final _aboutGlobaleKey = GlobalKey();
+  final _statisticsGlobaleKey = GlobalKey();
+  final _workingProcessGlobaleKye = GlobalKey();
+  final _recentProjectsGlobaleKey = GlobalKey();
+  final _contactUsGlobaleKey = GlobalKey();
+
+  final _scrollController = ScrollController();
+
+  final _fabStream = StreamController<bool>();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      _fabStream.sink.add(_scrollController.offset > 500);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      desktopScreen: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/background.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: CustomScrollView(
+            controller: _scrollController,
+            slivers: [
+              SliverAppBar(
+                key: _headerGlobalKey,
+                titleSpacing: 0,
+                toolbarHeight: 100,
+                backgroundColor: Colors.transparent,
+                flexibleSpace: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('images/cover.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        colors: [
+                          Colors.black,
+                          Colors.black87,
+                          Colors.transparent
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                title: Padding(
+                  padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width * .15,
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(1000),
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      color: Colors.white,
+                      child: Image.asset('images/mc.png'),
+                    ),
+                  ),
+                ),
+                bottom: PreferredSize(
+                  preferredSize: Size.fromHeight(500),
+                  child: Header(),
+                ),
+                actions: [
+                  Row(
+                    children: [
+                      MaterialButton(
+                        onPressed: _scrollToAbout,
+                        highlightColor: Colors.white60,
+                        child: Text(
+                          'About Me',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      MaterialButton(
+                        onPressed: _scrollToStatistics,
+                        child: Text(
+                          'Experience',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      MaterialButton(
+                        onPressed: _scrollToWorkingProcess,
+                        child: Text(
+                          'Work Plan',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      MaterialButton(
+                        onPressed: _scrollToRecentProjects,
+                        child: Text(
+                          'Portfolio',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.deepPurple,
+                          onPrimary: Colors.white,
+                        ),
+                        onPressed: _scrollToContactUs,
+                        child: Text(
+                          'Contact Me',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: MediaQuery.of(context).size.width * .15),
+                ],
+              ),
+              ..._slivers(),
+            ],
+          ),
+        ),
+        floatingActionButton: _buildFab(),
+      ),
+      mobileScreen: Scaffold(
+        drawer: Drawer(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: const EdgeInsets.symmetric(vertical: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(1000),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(1000),
+                    child: Image.asset(
+                      'images/munya.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Divider(),
+                ListTile(
+                  onTap: _scrollToAbout,
+                  title: Text(
+                    'About Me',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ListTile(
+                  onTap: _scrollToStatistics,
+                  title: Text(
+                    'Experience',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ListTile(
+                  onTap: _scrollToWorkingProcess,
+                  title: Text(
+                    'Work Plan',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ListTile(
+                  onTap: _scrollToRecentProjects,
+                  title: Text(
+                    'Portfolio',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Divider(),
+                const SizedBox(height: 20),
+                ListTile(
+                  title: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                          primary: Colors.deepPurple,
+                          onPrimary: Colors.white,
+                        ),
+                    onPressed: _scrollToContactUs,
+                    child: Text(
+                      'Contact Me',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+              ExternalLinkIcon(
+                linkIcon: 'icons/linkedin.png',
+                linkURL: 'https://zw.linkedin.com/in/munyaradzi-chigangawa-45170818b',
+              ),
+              ExternalLinkIcon(
+                linkIcon: 'icons/github.png',
+                linkURL: 'https://github.com/Munyaradzi-Chigangawa',
+              ),
+              ExternalLinkIcon(
+                linkIcon: 'icons/twitter.png',
+                linkURL:
+                    'https://www.twitter.com/mchigangawa',
+              ),
+              ExternalLinkIcon(
+                linkIcon: 'icons/fb.png',
+                linkURL:
+                    'https://www.facebook.com/people/Munyaradzi-Chigangawa/100005882974770/',
+              ),
+            ],
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/background.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: CustomScrollView(
+            controller: _scrollController,
+            slivers: [
+              SliverAppBar(
+                key: _headerGlobalKey,
+                titleSpacing: 0,
+                centerTitle: true,
+                backgroundColor: Colors.transparent,
+                leading: Align(
+                  child: Builder(
+                    builder: (ctx) => InkWell(
+                      onTap: () {
+                        Scaffold.of(ctx).openDrawer();
+                      },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(1000),
+                        child: Container(
+                          width: 40,
+                          height: 40,
+                          color: Colors.white,
+                          child: Image.asset('images/munya.png'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                flexibleSpace: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('images/cover.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        colors: [
+                          Colors.black,
+                          Colors.black87,
+                          Colors.transparent
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                bottom: PreferredSize(
+                  preferredSize: Size.fromHeight(350),
+                  child: Header(),
+                ),
+              ),
+              ..._slivers(),
+            ],
+          ),
+        ),
+        floatingActionButton: _buildFab(),
+      ),
+    );
+  }
+
+  List<Widget> _slivers() => [
+        SliverToBoxAdapter(
+          key: _aboutGlobaleKey,
+          child: About(),
+        ),
+        SliverToBoxAdapter(
+          key: _statisticsGlobaleKey,
+          child: Statistics(),
+        ),
+        SliverToBoxAdapter(
+          key: _workingProcessGlobaleKye,
+          child: WorkingProcess(),
+        ),
+        SliverToBoxAdapter(
+          key: _recentProjectsGlobaleKey,
+          child: MyProjects(),
+        ),
+        SliverToBoxAdapter(
+          key: _contactUsGlobaleKey,
+          child: ContactUs(),
+        ),
+        SliverToBoxAdapter(
+         child: Footer1(),
+        ),
+      ];
+
+  Widget _buildFab() {
+    return StreamBuilder<bool>(
+      stream: _fabStream.stream,
+      builder: (_, data) {
+        final bool showFab = data.hasData && data.data;
+        return AnimatedOpacity(
+          opacity: showFab ? 1 : 0,
+          duration: const Duration(milliseconds: 500),
+          child: FloatingActionButton(
+            backgroundColor: Colors.deepPurple,
+            onPressed: showFab
+                ? _scrollToHeader
+                : null, // make sure user cannot click when button hidden
+            mini: true,
+            child: Image.asset('icons/uparrow.png',
+            width: 20,
+            height: 20,),
+          ),
+        );
+      },
+    );
+  }
+
+  void _scrollToHeader() {
+    Scrollable.ensureVisible(
+      _headerGlobalKey.currentContext,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
+  void _scrollToAbout() {
+    Scrollable.ensureVisible(
+      _aboutGlobaleKey.currentContext,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
+  void _scrollToStatistics() {
+    Scrollable.ensureVisible(
+      _statisticsGlobaleKey.currentContext,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
+  void _scrollToWorkingProcess() {
+    Scrollable.ensureVisible(
+      _workingProcessGlobaleKye.currentContext,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
+  void _scrollToRecentProjects() {
+    Scrollable.ensureVisible(
+      _recentProjectsGlobaleKey.currentContext,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
+  void _scrollToContactUs() {
+    Scrollable.ensureVisible(
+      _contactUsGlobaleKey.currentContext,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
+  @override
+  void dispose() {
+    _fabStream.close();
+    super.dispose();
+  }
+}
