8a6a6ee6ade5397776df9578d45270b0b78c9118
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-07-13 02:41:54 +0200

    misc

---
+++ lib/config/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+mixin AppStyles {
+  static final TextStyle title = TextStyle(
+    color: Colors.black.withOpacity(.8),
+    fontSize: 35,
+    fontWeight: FontWeight.w700,
+  );
+}
