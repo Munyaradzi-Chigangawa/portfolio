5ad638775e0530efc49e3c736c1db5feabd705a9
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-07-22 02:43:28 +0200

    added skills

---
+++ lib/data/skills.dart
@@ -0,0 +1,14 @@
+class Skill {
+  final String name;
+
+  Skill({this.name});
+}
+
+// ignore: non_constant_identifier_names
+List<Skill> SKILLS = [
+  Skill(name: 'Adobe XD'),
+  Skill(name: 'Firebase'),
+  Skill(name: 'Dart'),
+  Skill(name: 'Flutter'),
+  Skill(name: 'Git'),
+];
